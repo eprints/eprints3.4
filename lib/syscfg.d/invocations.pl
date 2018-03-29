81578e1c41dbf12276cfde6c7562ec8e6665d48f
Jiadi Yao <jy2e08@ecs.soton.ac.uk>
2018-03-29 15:27:49 +0100

    update licenses for lib/syscfg.d scripts

--- lib/syscfg.d/invocations.pl
+++ lib/syscfg.d/invocations.pl
@@ -38,10 +38,9 @@ while(my( $name, $invo ) = each %invocations)
 
 =for COPYRIGHT BEGIN
 
-Copyright 2017 University of Southampton.
+Copyright 2018 University of Southampton.
 EPrints 3.4 is supplied by EPrints Services.
 
-This software may be used with permission and must not be redistributed.
 http://www.eprints.org/eprints-3.4/
 
 =for COPYRIGHT END

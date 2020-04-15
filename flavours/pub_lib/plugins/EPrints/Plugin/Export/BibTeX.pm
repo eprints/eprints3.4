8df96d8c19f09de1c136013c5d716f06cd35fb51
Pfiffikus <2845742+Pfiffikus@users.noreply.github.com>
2020-04-15 19:19:49 +0200

    switch to robust utf-8-export of 'author' (#25)
    given 'official_url' overwrites repoURL, output of IS[SB]N is added too

--- flavours/pub_lib/plugins/EPrints/Plugin/Export/BibTeX.pm
+++ flavours/pub_lib/plugins/EPrints/Plugin/Export/BibTeX.pm
@@ -32,10 +32,82 @@ sub new
 	$self->{accept} = [ 'list/eprint', 'dataobj/eprint' ];
 	$self->{visible} = "all";
 	$self->{suffix} = ".bib";
-	$self->{mimetype} = "text/plain";
+	$self->{mimetype} = "text/plain; charset=utf-8";
 	unshift @{$self->{produce}}, "application/x-bibtex";
 
 	return $self;
+}
+
+######################################################################
+
+=pod
+
+=item $string = make_bibtex_string( $name )
+
+Return a string containing the name described in the hash reference
+$name for BibTeX
+
+The keys of the hash are one or more of given, family, honourific and
+lineage. The values are utf-8 strings.
+
+The result will be in the most robust format "family, lineage, honourific given";
+cf. https://tex.stackexchange.com/questions/557/how-should-i-type-author-names-in-a-bib-file
+=cut
+
+######################################################################
+
+sub make_bibtex_string
+{
+	my( $name ) = @_;
+
+	return "make_bibtex_string expected hash reference" unless ref($name) eq "HASH";
+
+	my $tmp = "";
+	my $tail = "";
+	if( defined $name->{honourific} && $name->{honourific} ne "" )
+	{
+		$tmp = $name->{honourific};
+		$tmp =~ s/(.+?),/$1\{,\}/g;  # enclose any comma in curly braces
+		$tmp =~ s/\band\b/\{and\}/g;  # enclose any single 'and' in curly braces
+		$tail = $tmp." ";
+	}
+	if( defined $name->{given} )
+	{
+		$tmp = $name->{given};
+		$tmp =~ s/(.+?),/$1\{,\}/g;  # enclose any comma in curly braces
+		$tmp =~ s/\band\b/\{and\}/g;  # enclose any single 'and' in curly braces
+		$tmp =~ s/\.([A-Z])/. $1/g;  # separate sloughed initials
+		$tmp =~ s/^[\s]*$/\{\}/g;  # replace empty 'given' token
+	}
+	else
+	{
+		$tmp = "{}";  # ensure 'given' token
+	}
+	$tail .= $tmp;
+	
+	$tmp = "{}";
+	my $lineage = "";
+	if( defined $name->{family} )
+	{
+		$lineage = $tmp = $name->{family};
+		$tmp =~ s/(.+?)((,| )\s*[IVX]+\.?)*((,| )\s*[jJsS](un|en)?(io)?r\.?)*((,| )\s*[IVX]+\.?)*$/$1/g;  # remove contained lineage
+		$lineage =~ s/\Q${tmp}\E,?\s*(.*)$/$1/g;  # save contained lineage, but pay attention to metacharacters
+		$tmp =~ s/(.+?),/$1\{,\}/g;  # enclose any comma in curly braces
+		$tmp =~ s/\band\b/\{and\}/g;  # enclose any single 'and' in curly braces
+	}
+	$tmp =~ s/^[\s]*$/\{\}/g;  # ensure 'last' token
+	my $head = $tmp;
+	$head .= ", ".$lineage if $lineage ne "";
+
+	if( defined $name->{lineage} && $name->{lineage} ne "" )
+	{
+		$tmp = $name->{lineage};
+		$tmp =~ s/(.+?),/$1\{,\}/g;  # enclose any comma in curly braces
+		$tmp =~ s/\band\b/\{and\}/g;  # enclose any single 'and' in curly braces
+		$head .= ", " if $lineage eq "";
+		$head .= " ".$tmp;
+	}
+	return $head.", ".$tail;
 }
 
 sub convert_dataobj
@@ -90,7 +162,7 @@ sub convert_dataobj
 	if( $dataobj->exists_and_set( "creators" ) )
 	{
 		my $names = $dataobj->get_value( "creators" );	
-		$data->{bibtex}->{author} = join( " and ", map { EPrints::Utils::make_name_string( $_->{name}, 1 ) } @$names );
+		$data->{additional}->{author} = join( " and ", map { make_bibtex_string( $_->{name} ) } @$names );
 	}
 	
 	# booktitle
@@ -180,7 +252,12 @@ sub convert_dataobj
 	# Not part of BibTeX
 	$data->{additional}->{abstract} = $dataobj->get_value( "abstract" ) if $dataobj->exists_and_set( "abstract" );
 	$data->{additional}->{url} = $dataobj->get_url(); 
+	$data->{additional}->{url} = $dataobj->get_value( "official_url" ) if $dataobj->exists_and_set( "official_url" );
+
 	$data->{additional}->{keywords} = $dataobj->get_value( "keywords" ) if $dataobj->exists_and_set( "keywords" );
+
+	$data->{additional}->{issn} = $dataobj->get_value( "issn" ) if $dataobj->exists_and_set( "issn" );
+	$data->{additional}->{isbn} = $dataobj->get_value( "isbn" ) if $dataobj->exists_and_set( "isbn" );
 
 	return $data;
 }
@@ -207,6 +284,10 @@ sub output_dataobj
 		{
 			$value = encode('bibtex', $value );
 		}
+		$value =~ s/\\\{\\\}/\{\}/g if( $k eq "author" );  # 'decode' deliberately inserted code
+		$value =~ s/\\\{,\\\}/\{,\}/g if( $k eq "author" );
+		$value =~ s/\\\{and\\\}/\{and\}/g if( $k eq "author" );
+
 		push @list, sprintf( "%16s = {%s}", $k, $value );
 	}
 
@@ -248,4 +329,3 @@ License along with EPrints 3.4.
 If not, see L<http://www.gnu.org/licenses/>.
 
 =for LICENSE END
-

b2c883848debdbd11ef01c94417de5369b5c9c93
David R Newman <drn@ecs.soton.ac.uk>
2022-01-24 17:07:04 +0000

    Added merged set of functions for RDF URIs.

--- lib/cfg.d/rdf_uris.pl
+++ lib/cfg.d/rdf_uris.pl
@@ -1,3 +1,29 @@
+# event_uri
+$c->{rdf}->{event_uri} = sub {
+    my( $eprint ) = @_;
+
+    return if( !$eprint->dataset->has_field( "event_title" ) );
+
+    my $ev_title = $eprint->get_value( "event_title" );
+    return if( !defined $ev_title  );
+
+    my $ev_dates = "";
+    if( $eprint->dataset->has_field( "event_dates" ) )
+    {
+        $ev_dates = $eprint->get_value( "event_dates" ) || "";
+    }
+
+    my $ev_location = "";
+    if( $eprint->dataset->has_field( "event_location" ) )
+    {
+        $ev_location = $eprint->get_value( "event_location" ) || "";
+    }
+
+    my $raw_id = "eprintsrdf/$ev_title/$ev_location/$ev_dates";
+    utf8::encode( $raw_id ); # md5 takes bytes, not characters
+
+    return "epid:event/ext-".md5_hex( $raw_id );
+};
 
 # event location uri
 $c->{rdf}->{event_location_uri} = sub {
@@ -12,7 +38,59 @@ $c->{rdf}->{event_location_uri} = sub {
 	return "epid:location/ext-".md5_hex( $ev_location );
 };
 	
+# org_uri
+$c->{rdf}->{org_uri} = sub {
+    my( $eprint, $org_name ) = @_;
 
+    return if( !$org_name );
+
+    my $raw_id = "eprintsrdf/$org_name";
+    utf8::encode( $raw_id ); # md5 takes bytes, not characters
+
+    return "epid:org/ext-".md5_hex( $raw_id );
+};
+
+# person_uri
+$c->{rdf}->{person_uri} = sub {
+    my( $eprint, $person ) = @_;
+
+    my $repository = $eprint->repository;
+    if( EPrints::Utils::is_set( $person->{id} ) )
+    {
+        # If you want to use hashed ID's to prevent people reverse engineering
+        # them from the URI, uncomment the following line and edit SECRET to be
+        # something unique and unguessable.
+        #
+        # return "epid:person/ext-".md5_hex( utf8::encode( $person->{id}." SECRET" ));
+
+        return "epid:person/ext-".$person->{id};
+    }
+
+    my $name = $person->{name};
+    my $code = "eprintsrdf/".$eprint->get_id."/".($name->{family}||"")."/".($name->{given}||"");
+    utf8::encode( $code ); # md5 takes bytes, not characters
+    return "epid:person/ext-".md5_hex( $code );
+};
+
+# publication_uri
+$c->{rdf}->{publication_uri} = sub {
+    my( $eprint ) = @_;
+
+    my $repository = $eprint->repository;
+    if( $eprint->dataset->has_field( "issn" ) && $eprint->is_set( "issn" ) )
+    {
+        my $issn = $eprint->get_value( "issn" );
+        $issn =~ s/[^0-9X]//g;
+
+        return "epid:publication/ext-$issn";
+    }
+
+    return if( !$eprint->is_set( "publication" ) );
+
+    my $code = "eprintsrdf/".$eprint->get_value( "publication" );
+    utf8::encode( $code ); # md5 takes bytes, not characters
+    return "epid:publication/ext-".md5_hex( $code );
+};
 
 
 =head1 COPYRIGHT

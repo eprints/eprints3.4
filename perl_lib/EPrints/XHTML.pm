e5e071ed369ea712f6cdecebc35a3b9b9ed0b4d3
Edward Oakley <47759261+fatchild@users.noreply.github.com>
2022-09-18 11:05:57 +0100

    View details table class attributes (#252)
    * View details table class attributes
    
    * DN Suggestion of more general name for tabs class

--- perl_lib/EPrints/XHTML.pm
+++ perl_lib/EPrints/XHTML.pm
@@ -839,6 +839,7 @@ sub tabs
 			$labels->[$_],
 			href => $href,
 			onclick => "return ep_showTab('$basename','$label',".($expensive{$_}?1:0).");",
+			class => "ep_tab_link",
 		) );
 
 		if( defined $panel )

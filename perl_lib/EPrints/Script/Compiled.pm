2298a35726dd76abaee81130241969c03903c17a
David R Newman <drn@ecs.soton.ac.uk>
2023-12-12 16:05:15 +0000

    Allows epc calls for large icons and outputting phrase as just text rather than HTML

--- perl_lib/EPrints/Script/Compiled.pm
+++ perl_lib/EPrints/Script/Compiled.pm
@@ -651,7 +651,7 @@ sub run_icon
 		elsif( $optv eq "NewWindow" ) { $args{new_window}=1; }
 		elsif( $optv eq "noNewWindow" ) { $args{new_window}=0; }
 		elsif( $optv eq "previewLeft" ) { $args{preview_side}='left'; }
-		elsif( $optv eq "previewRight" ) { $args{preview_side}='right'; }
+		elsif( $optv eq "LargeIcon" ) { $args{size}='lightbox'; }
 		else { $self->runtime_error( "Unknown option to doc->icon(): $optv" ); }
 	}
 
@@ -929,6 +929,13 @@ sub run_phrase
 	my( $self, $state, $phrase ) = @_;
 
 	return [ $state->{session}->html_phrase( $phrase->[0] ), "XHTML" ];
+}
+
+sub run_text
+{
+	my( $self, $state, $phrase ) = @_;
+
+	return [ $state->{session}->make_text( $phrase->[0] ), "XHTML" ];
 }
 
 sub run_documents

62f45a3c677e632c40d4533b4ae23eb453b4d3f7
Edward Oakley <47759261+fatchild@users.noreply.github.com>
2023-12-20 12:09:08 +0000

    tabs not spaces

--- perl_lib/EPrints/Plugin/Screen/Subject/Edit.pm
+++ perl_lib/EPrints/Plugin/Screen/Subject/Edit.pm
@@ -507,14 +507,14 @@ sub action_remove
 	# already removed?
 	return if !defined $child;
 
-    if ($child->remove())
-    {
-        $self->{processor}->add_message( "message", $self->html_phrase( "removed" ) );
-    }
-    else
-    {
-        $self->{processor}->add_message( "warning", $self->html_phrase( "has_child" ) );
-    }
+	if ($child->remove())
+	{
+		$self->{processor}->add_message( "message", $self->html_phrase( "removed" ) );
+	}
+	else
+	{
+		$self->{processor}->add_message( "warning", $self->html_phrase( "has_child" ) );
+	}
 }
 
 sub from

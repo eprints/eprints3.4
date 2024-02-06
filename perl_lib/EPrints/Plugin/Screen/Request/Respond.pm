9262b7914cfeb084502caf1c257ba9764772581e
David R Newman <drn@ecs.soton.ac.uk>
2024-02-06 15:52:30 +0000

    Fixes #360

--- perl_lib/EPrints/Plugin/Screen/Request/Respond.pm
+++ perl_lib/EPrints/Plugin/Screen/Request/Respond.pm
@@ -338,13 +338,16 @@ sub render
 		{
 			my $p = $session->make_element( "p" );
 			$form->appendChild( $p );
-			my $label = $session->make_element( "label" );
-			my $cb = $session->make_element( "input", type => "checkbox", name => "oa" );
-			$label->appendChild( $cb );
-			$label->appendChild( $session->make_text( " " ));
-			$label->appendChild( $session->html_phrase(
-				"request/respond_page:fieldname_oa" ) );
-			$p->appendChild( $label );
+			unless ( $session->config( 'disable_make_open_access' ) )
+			{
+				my $label = $session->make_element( "label" );
+				my $cb = $session->make_element( "input", type => "checkbox", name => "oa" );
+				$label->appendChild( $cb );
+				$label->appendChild( $session->make_text( " " ));
+				$label->appendChild( $session->html_phrase(
+					"request/respond_page:fieldname_oa" ) );
+				$p->appendChild( $label );
+			}
 		}
 
 		$form->appendChild( $session->render_hidden_field( "screen", $self->{processor}->{screenid} ) );

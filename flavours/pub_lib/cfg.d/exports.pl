7a7ccda6e0c4c330272fe8b4a9de3ac4122f25ac
David R Newman (EPrints 3.4 development) <drn@ecs.soton.ac.uk>
2026-03-02 16:29:46 +0000

    Fixes #561 by allowing bespoke config of access to staff export plugins

--- flavours/pub_lib/cfg.d/exports.pl
+++ flavours/pub_lib/cfg.d/exports.pl
@@ -1,1 +1,8 @@
 $c->{export}->{publication_status_type_override} = 1;
+
+$c->{export}->{staff_check} = sub {
+
+	my ( $session, $user ) = @_;
+
+	return $user->get_type eq "editor" || $user->get_type eq "admin" || $user->get_type eq "local_admin"; 
+};

76ce2a2bffb37e8fe8c6fef9e2938632700720a7
David R Newman (EPrints 3.4 development) <drn@ecs.soton.ac.uk>
2026-03-18 12:25:02 +0000

    Better comment for config function for staff Export plugin access (#561)

--- flavours/pub_lib/cfg.d/exports.pl
+++ flavours/pub_lib/cfg.d/exports.pl
@@ -1,6 +1,6 @@
 $c->{export}->{publication_status_type_override} = 1;
 
-# Same as $user->is_staff
+# Below is an equivalent configuration function to $user->is_staff.  If you need to allow access to 'staff' restricted Export plugins to users other than admins and editors, then uncomment the configuration function below and edit as appropriate.
 #c->{export}->{staff_check} = sub {
 #
 #   my ( $session, $user ) = @_;

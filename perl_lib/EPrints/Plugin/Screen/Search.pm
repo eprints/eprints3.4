5d4c9ec8cb8ee43226d8d7bb9a650e092c7c3cb0
Luke Wallin <lpw1r25@soton.ac.uk>
2026-09-01 13:39:33 +0000

    Remove "?template=default" from end of all links on advanced search.

--- perl_lib/EPrints/Plugin/Screen/Search.pm
+++ perl_lib/EPrints/Plugin/Screen/Search.pm
@@ -158,7 +158,7 @@ sub render_result_row
 
 	#if we have a template, add it on the end of the item's url
 	my %params;
-	if( defined $self->{processor}->{sconf}->{template} )
+	if( defined $self->{processor}->{sconf}->{template} && $self->{processor}->{sconf}->{template} ne 'default')
 	{
 		$params{url} = $result->url . "?template=" . $self->{processor}->{sconf}->{template};
         }

43c0253986d8a93e5174d923738361c694297acc
Will Fyson <will.fyson@cosector.com>
2026-09-12 10:00:29 +0000

    Applying same fix to similar code elsewhere

--- perl_lib/EPrints/Page.pm
+++ perl_lib/EPrints/Page.pm
@@ -80,7 +80,7 @@ sub send
 	if( $@ )
 	{
 		if(
-			$@ !~ m/^Software caused connection abort/ &&
+            $@ !~ m/:Apache2 IO write: \(103\) Software caused connection abort/ &&
 			$@ !~ m/:Apache2 IO write: \(104\) Connection reset by peer/ &&
 			$@ !~ m/:Apache2 IO write: \(32\) Broken pipe/ &&
 			$@ !~ m/:Apache2 IO write: \(70007\) The timeout specified has expired/

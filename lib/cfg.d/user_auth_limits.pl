37e68858597eeb9398577380f0f374982cb51224
David R Newman (EPrints 3.4 development) <drn@ecs.soton.ac.uk>
2026-09-23 12:11:56 +0000

    Fixes #614 by requiring new password re-entering for verification

--- lib/cfg.d/user_auth_limits.pl
+++ lib/cfg.d/user_auth_limits.pl
@@ -1,7 +1,7 @@
 $c->{max_login_attempts} = 10;
 $c->{lockout_minutes} = 10;
 
-$c->{reset_request_recent_hours} = 24;
+$c->{reset_request_recent_hours} = 1;
 
 $c->{max_account_requests} = 100;
 $c->{max_account_requests_minutes} = 60;
